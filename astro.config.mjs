2f8f0892d2eafdf3c7dfdafcd2b6489b0aea16fe
prateekjain98 <prateek9jain8@gmail.com>
2025-07-21 16:42:55 +0530

    sitemap config updates

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,6 @@ import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://astro-starter-template-alpha.vercel.app/',
   integrations: [sitemap()],
 });
